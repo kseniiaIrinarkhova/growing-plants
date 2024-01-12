b9e991e5e761be8b9060cd09c5c5fb6b82a8d2cd
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-01-12 15:03:13 -0800

    task explanation

---
+++ index.js
@@ -0,0 +1,31 @@
+/**--------------------Input
+* The area in which the plants are contained is circular, with a radius of 5 meters.
+* The formula for calculating the area of a circle is PI multiplied by the radius, squared:const PI = 3.1415;const area = PI * radius * radius;
+* Each plant requires a minimum space of 0.8 square meters.
+* The area is starting with 20 plants.
+* The plants double in number every week.
+ */
+
+/**--------------------Part 1
+ * Predict the plant growth after a specific number of weeks.
+* Implement control flow to make decisions on whether the plants should be:
+*   Pruned, to stop them from exceeding the capacity of the garden.
+*          This condition should be met if the plant count after the given number of weeks is greater than 80% of the maximum capacity of the garden.
+*   Monitored, if they are growing at an acceptable rate.
+*          This condition should be met if the plant count is between 50% and 80% of the maximum capacity of the garden after the given number of weeks.
+*   Planted, if there is room to plant more plants.
+*          This condition should be met if the plant count after the given number of weeks is less than 50% of the maximum capacity of the garden.
+*Within your submission, include the results for 1, 2, and 3 weeks of growth as inputs.
+ */
+
+/**---------------------Part 3
+ * The conservation area in which the garden is located has multiple other gardens. 
+*Using the logic you have already created, determine:
+*The amount of additional space that would be required if the scientists were to start with 100 plants, and did not prune them for 10 weeks.
+*If the space remained circular, what would be the radius of this expanded garden?
+ */
+
+/**---------------------Part 4
+* The scientists decided not to listen to your recommendations, and have instead started with 100 plants in the original 5-meter-radius garden.
+* Use try and catch to wrap your work in an error-handling block. If the amount of space required to hold the originally provided number of plants exceeds the amount of space available, throw a new error and log an appropriate message.
+ */
