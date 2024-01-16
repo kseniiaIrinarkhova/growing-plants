906e6ea53075ba8e0ba36677ba83673df4fbdb6a
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-01-15 18:29:23 -0800

    Part 3 of task

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ const maxCapacity = area/minPlantSpace; //count max garden capacity
 let weekNumber = 2;
 
 let plantAmount = startPlantAmount*(2**weekNumber); //count how many plants will be after viewed week
-//let plantArea = plantAmount*minPlantSpace; //calc the area which current plant's ammount 
 let occupancy = plantAmount / maxCapacity * 100; //calc current plant occupancy
 
 if (occupancy > 80) {
@@ -44,13 +43,26 @@ if (occupancy > 80) {
 }
 else{
     console.log(`Monitored! currently plants occupy ${occupancy} of the garden.`)
-}
+};
 /**---------------------Part 3
  * The conservation area in which the garden is located has multiple other gardens. 
 *Using the logic you have already created, determine:
 *The amount of additional space that would be required if the scientists were to start with 100 plants, and did not prune them for 10 weeks.
 *If the space remained circular, what would be the radius of this expanded garden?
  */
+console.log(`
+****************************
+*******next part************
+****************************
+`);
+
+startPlantAmount = 100;
+weekNumber = 10;
+plantAmount = startPlantAmount * (2 ** weekNumber); //count how many plants will be after viewed week
+let plantArea = plantAmount*minPlantSpace; //calc the area which current plant's ammount 
+console.log(`After ${weekNumber} weeks plants will occupy ${plantArea.toFixed(2)} square meters. It needs ${(plantArea - area).toFixed(2)} square meters more.`);
+radius = Math.sqrt(plantArea/PI);
+console.log(`The radius of the garden would be ${radius.toFixed(2)} meters`);
 
 /**---------------------Part 4
 * The scientists decided not to listen to your recommendations, and have instead started with 100 plants in the original 5-meter-radius garden.
